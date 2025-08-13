5df49c7563aac866d3b50e1a954afceb6ba5d084
Mauro <mfelipxl@gmail.com>
2025-08-12 22:35:44 -0400

    feat: add styles to header on cardId page.

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useBoardContext } from "../utils/useBoardContext"
+import styles from "../styles/boardsListPage.module.css"
+
 
 export const CardPage = () => {
     const params = useParams()
@@ -10,8 +12,11 @@ export const CardPage = () => {
     const card = columns?.cards.find((c) => c.id == cardId) 
 
     return (
-        <div>
-            <h1>Card: {card?.title}</h1>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1 className={styles.title}>Card: {card?.title}</h1>
+            </div>
         </div>
+
     )
 }
